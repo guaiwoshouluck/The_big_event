478c1a0d69aab74d90655a36ea593a72f638d09a
github <w1401752250@qq.com>
2022-04-26 22:38:36 +0800

    init Invoke interface Interface for initiating registered users

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,6 +16,31 @@ $(function() {
         //通过form.verify()函数自定义校验规则
     form.verify({
         //自定义 pwd 的校验规则
-        pwd: [/^[\S]{6,12}$/, '密码必须在6-12范围内，且不能有空格']
+        pwd: [/^[\S]{6,12}$/, '密码必须在6-12范围内，且不能有空格'],
+        //校验两次密码是否一致规则
+        //通过形参获取确认密码框中的内容
+        repwd: function(value) {
+            //拿到密码框内容
+            var pwd = $('.reg-box [name=password]').val()
+                // 判断内容是否相同
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+            //不一致返回提示结果
+        }
+    })
+
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault()
+        $.post('http://www.liulongbin.top:3007/api/reguser', {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }, function(res) {
+            if (res.status !== 0) {
+                return console.log(res.message)
+            }
+            console.log('注册成功！')
+        })
     })
 })
